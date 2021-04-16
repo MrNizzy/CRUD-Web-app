ba835ebbb732070e447960709ae3c26f8505f96a
Megof <j_do_o13@hotmail.com>
2021-04-16 13:53:54 -0500

    correción tabla historial

--- assets/conection/registro.php
+++ assets/conection/registro.php
@@ -13,20 +13,21 @@
 		if(mysqli_num_rows($consulta) > 0){
 			$sentencia = "SELECT * from mascotas where id_owner  = '$busqueda'";
     		$consulta = mysqli_query($conex, $sentencia);
-			$array = mysqli_fetch_array($consulta);
+			//$array = mysqli_fetch_array($consulta);
 			$cantidad=mysqli_num_rows($consulta);
 			if($cantidad > 0){
 				$_SESSION['pet']="";
-				for($i=0;$i<$cantidad;$i++){
+				while($row = $consulta->fetch_array()){
 					$_SESSION['pet']=$_SESSION['pet'].'<tr>
-                                <th scope="row">'.$array['nombre']."</th>
+                                <th scope="row">'.$row['nombre']."</th>
                                 <td>".$array1['nombre']."</td>
-                                <td>".$array1['dueno']['telefono']."</td>
+                                <td>".$array1['telefono']."</td>
                                 <td class='mx-auto' style='width: 200px;'>
                             <button class='btn btn-outline-main mb-3' type='submit'>Modificar</button>
                             <button class='btn btn-outline-danger mb-3' type='button' onclick='eliminar()'>Eliminar</button>
                         </td>";
-                }
+				}
+                        echo ($_SESSION['pet']);
     			header("location: ../../p-clinico.php");
 			}else{
 				echo("Error");
