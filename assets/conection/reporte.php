abda2bf597a0730738830f23dd736b1cc311c867
Megof <j_do_o13@hotmail.com>
2021-04-17 00:09:12 -0500

    añadida variable para pdf

--- assets/conection/reporte.php
+++ assets/conection/reporte.php
@@ -4,6 +4,7 @@
 	$fecha1 = $_GET['fecha1'];
 	$fecha2 = $_GET['fecha2'];
 	$_SESSION['factura']="";
+	/**/$_SESSION['pdf']="";
 	$conex=Conectarse();
     if($conex==false){
     	echo("error");
@@ -26,7 +27,8 @@
 						$sqlowner = "SELECT * FROM owner WHERE id = '$owner'";
 					    $consultaowner = mysqli_query($conex, $sqlowner);
 						$arrayowner = mysqli_fetch_array($consultaowner);
-						if(mysqli_num_rows($consultaowner) > 0){					
+						if(mysqli_num_rows($consultaowner) > 0){
+							/**/$_SESSION['pdf']=$_SESSION['pdf'].";".$arraymascota['nombre'].";".$arrayowner['nombre'].";".$arrayprocedimiento['procedimiento'].";".$arrayprocedimiento['medicamentos'].";".$row['fecha'].";".$row['costo']."\n";					
 							$_SESSION['factura']=$_SESSION['factura']."<tr><th scope='row'>".$arraymascota['nombre']."</th>
 	                        <td>".$arrayowner['nombre']."</td>
 	                        <td>".$arrayprocedimiento['procedimiento']."</td>
